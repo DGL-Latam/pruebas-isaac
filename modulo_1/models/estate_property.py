ea807bdcf4819f89cd4cdc2f1126772b8de6c5d1
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-01-18 10:13:21 -0600

    Modulo 2

--- modulo_1/models/estate_property.py
+++ modulo_1/models/estate_property.py
@@ -30,9 +30,14 @@ class EstateProperty(models.Model):
 
     active = fields.Boolean("Active", default=True)
 
-    property_type = fields.Many2one("estate.property.type", string="Property Types")
+    property_type_id = fields.Many2one("estate.property.type", string="Property Types")
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
 
     name = fields.Char("Property Types", required=True)
+    property_type = fields.Selection(string="Property Type", selection=[("house", "House"), ("apartment", "Apartment"), ("penthouse", "Penthouse"), ("other", "Other")])
+    postcode = fields.Char("Postcode")
+    date_availability = fields.Date("Available From", default=three_months, required=True, copy=False)
+    expected_price = fields.Float("Expected Price", required=True)
+    selling_price = fields.Float("Selling Price", required=True, readonly=False, copy=False)
