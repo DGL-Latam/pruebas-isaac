ba5985c47f0e7000ccc0f3b23744c7b6e46af33b
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-01-18 08:41:37 -0600

    Test type

--- modulo_1/models/estate_property_type.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-from odoo.tools import date_utils
-
-class EstatePropertyType(models.Model):
-    _name = "estate.property.type"
-    _description = "Estate Property Type"
-
-    name = fields.Char("Title", requiered=True)
