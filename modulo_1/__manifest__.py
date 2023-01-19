dce2b2a2b647441407b760d17c9bede0652b22d5
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-01-19 13:54:02 -0600

    Test

--- modulo_1/__manifest__.py
+++ modulo_1/__manifest__.py
@@ -8,8 +8,8 @@
     'description': "",
     'data': [
         'security/ir.model.access.csv',
-        #'views/estate_property_views.xml',
-        #'views/estate_property_views_2.xml'
+        'views/estate_property_views.xml',
+        'views/estate_property_views_2.xml'
 
     ]
 }
