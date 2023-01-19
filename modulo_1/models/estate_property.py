c134640399b91d6024d1f35f69d0609c57273cc3
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-01-19 09:09:27 -0600

    Test

--- modulo_1/models/estate_property.py
+++ modulo_1/models/estate_property.py
@@ -30,7 +30,7 @@ class EstateProperty(models.Model):
 
     active = fields.Boolean("Active", default=True)
 
-    property_type_id = fields.Many2one("estate.property.type", string="Property Type", ondelete="cascade")
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type", ondelete="cascade", index=True)
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
