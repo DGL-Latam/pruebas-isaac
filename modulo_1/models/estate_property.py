dc2e4ad3a14f41a7d326c26d4b33d524632030e8
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-01-20 08:28:08 -0600

    Test

--- modulo_1/models/estate_property.py
+++ modulo_1/models/estate_property.py
@@ -32,8 +32,7 @@ class EstateProperty(models.Model):
 
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
 
-    user_id = fields.Many2one("res.users", string="Salesperson", index=True, tracking=True,
-                              default=lambda self: self.env.uid)
+    user_id = fields.Many2one("res.users", string="Salesperson")
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
 
