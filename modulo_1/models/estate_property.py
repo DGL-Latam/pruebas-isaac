28fddc60bceec19fa5562ebefee387ad3ea6d768
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-01-20 11:12:45 -0600

    Test

--- modulo_1/models/estate_property.py
+++ modulo_1/models/estate_property.py
@@ -50,5 +50,6 @@ class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
 
     price = fields.Float("Price")
-    status = fields.Selection("Status", copy=False, selection=[("accepted", "Accepted"), ("refused", "Refused")])
-    
+    status = fields.Selection(string="Status", copy=False, selection=[("accepted", "Accepted"), ("refused", "Refused")])
+    partner_id = fields.Many2one("res.partner", required=True)
+    property_id = fields.Many2one("estate.property", required=True)
