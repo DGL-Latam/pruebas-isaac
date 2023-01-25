30a7bdc1564503d8ca48171487aa9b0caa2909ba
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-01-25 16:19:00 -0600

    Test

--- modulo_1/models/estate_property.py
+++ modulo_1/models/estate_property.py
@@ -81,5 +81,5 @@ class EstatePropertyOffer(models.Model):
     @api.depends("create_date", "validity")
     def _computed_date_deadline(self):
         for record in self:
-            record.date_deadline = record.create_date.add(record.validity)
+            record.date_deadline = record.create_date + date_utils.add(record.create_date, days=record.validity)
 
