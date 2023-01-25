9d01fc50c759da76826a302da4bb8a758348f0b5
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-01-25 13:33:23 -0600

    Test

--- modulo_1/models/estate_property.py
+++ modulo_1/models/estate_property.py
@@ -50,7 +50,8 @@ class EstateProperty(models.Model):
     @api.depends("offer_ids.price")
     def _compute_best_offer(self):
         for record in self:
-            record.best_offer = max(self.mapped("offer_ids.price"))
+
+            record.best_offer = max(self.mapped(record.offer_ids.price))
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
 
