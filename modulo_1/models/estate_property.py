612bc6e1cc0af8ca597da20bad8ff21efa47b396
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-01-26 09:33:17 -0600

    Test

--- modulo_1/models/estate_property.py
+++ modulo_1/models/estate_property.py
@@ -87,6 +87,4 @@ class EstatePropertyOffer(models.Model):
     def _inverse_validity(self):
         for record in self:
             record.validity = fields.Integer()
-
-        for record in self:
             record.date_deadline = fields.Date()
